9c6125a0581b1d8081ba31b214fff0ab3b5c8fd8
Oliwier Moskala <oliwiermoskala2@gmail.com>
2026-09-03 18:57:58 +0200

    user rating home page section

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import HomeNavbar from "../../components/navbar/homeNavbar/HomeNavbar";
 import HomeTitleSection from "./sections/homeTitleSection/HomeTitleSection";
 import HomeHowItWorksSection from "./sections/homeHowItWorksSection/HomeHowItWorksSection";
 import HomePricePlansSection from "./sections/homePricePlansSection/HomePricePlansSection";
+import HomeUserRatingSection from "./sections/homeUserRatingSection/HomeUserRatingSection";
 import "./home.css";
 export default function Home()
 {
@@ -11,6 +12,7 @@ export default function Home()
             <HomeTitleSection />
             <HomeHowItWorksSection id="how-it-works"/>
             <HomePricePlansSection id="price-plans"/>
+            <HomeUserRatingSection id="opinions"/>
         </main>
     );
 }
---
+++ frontend/src/pages/home/sections/homeUserRatingSection/HomeUserRatingSection.tsx
@@ -0,0 +1,53 @@
+import UserRating from "../../../../components/userRating/UserRating";
+import FadeIn from "../../../../components/fadeIn/FadeIn";
+import "./homeUserRatingSection.css";
+
+interface HomePricePlansSectionProps
+{
+    id ?: string
+}
+
+export default function HomeUserRatingSection(props: HomePricePlansSectionProps)
+{
+    return(
+        <section className="home-user-rating-section" {...props}>
+            <FadeIn>
+                <h2>Co mówią klijenci?</h2>
+            </FadeIn>
+            <div className="ratings">
+                <FadeIn>
+                    <div>
+                        <UserRating
+                            starsRating={5}
+                            content="Bardzo polecam! Wszystko działa dokładnie tak, jak powinno, a korzystanie z serwisu jest naprawdę proste i wygodne."
+                            name="Kamil"
+                            place="Cieszyn"
+                        />
+                    </div>
+                </FadeIn>
+
+                <FadeIn>
+                    <div>
+                        <UserRating
+                            starsRating={4}
+                            content="Jestem bardzo zadowolona z usługi. Cały proces przebiegł sprawnie, a kontakt był szybki i bezproblemowy. Na pewno jeszcze skorzystam."
+                            name="Julia"
+                            place="Katowice"
+                        />
+                    </div>
+                </FadeIn>
+
+                <FadeIn>
+                    <div>
+                        <UserRating
+                            starsRating={4}
+                            content="Bardzo dobre doświadczenie. Strona jest przejrzysta, wszystko można szybko znaleźć, a realizacja przebiegła bez żadnych problemów."
+                            name="Mateusz"
+                            place="Bielsko-Biała"
+                        />
+                    </div>
+                </FadeIn>
+            </div>
+        </section>
+    )
+}
